bcffaf6ddef9e1489a094a18e2287449a7770838
Ivan <80658123+Ivaan1@users.noreply.github.com>
2026-02-25 00:40:43 +0100

    confg de la db

---
+++ src/config/mongo.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const dbConnect = () => {
+    const db_uri = process.env.DB_URI
+    mongoose.set('strictQuery', false)
+    try {
+        mongoose.connect(db_uri)
+            .then(() => {
+                console.log("Conexión exitosa a la base de datos");
+            })
+    } catch (error) {
+        console.error("Error conectando a la BD: ", error)
+    }
+    // Listen events
+    mongoose.connection.on("connected", () => console.log("Conectado a la BD"))
+    mongoose.connection.on("disconnected", () => console.log("Desconectado de la BD")) 
+    mongoose.connection.on("error", (error) => console.error("Error de conexión a la BD:", error))
+}
+
+module.exports = dbConnect
