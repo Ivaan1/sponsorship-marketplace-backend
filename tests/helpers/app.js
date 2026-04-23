66a623d91e3db8881200ce891c2b784ed6a51a9f
Ivan <80658123+Ivaan1@users.noreply.github.com>
2026-04-23 22:31:06 +0200

    setup testing infrastructure and environment config

---
+++ tests/helpers/app.js
@@ -0,0 +1,15 @@
+require('dotenv').config({ path: '.env.test', quiet: true })
+ 
+const express = require('express')
+const cors = require('cors')
+ 
+const app = express()
+ 
+app.use(cors())
+app.use(express.json())
+
+
+app.use('/api', require('../../src/routes'))
+ 
+module.exports = app
+ 
