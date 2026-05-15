657a22856f91d9fe51fff211010a226b2600f97d
Ivan <80658123+Ivaan1@users.noreply.github.com>
2026-05-15 18:25:49 +0200

    feat: Implementar middleware de roles
    Añadido el rol de Admin tambien

---
+++ src/middlewares/role.js
@@ -0,0 +1,24 @@
+import { handleHttpError } from "../utils/handleErrors";
+
+const checkRole = (roles) => (req, res, next) => {
+    try {
+        const { user } = req;
+
+        if (!user) {
+            return handleHttpError(res, "SESSION_NOT_FOUND", 401);
+        }
+
+        const userRole = user.role;
+
+        // Verificamos si el rol del usuario está incluido en el array de roles permitidos
+        if (!roles.includes(userRole)) {
+            return handleHttpError(res, "FORBIDDEN_PERMISSIONS", 403);
+        }
+
+        next();
+    } catch (e) {
+        handleHttpError(res, "ERROR_CHECK_ROLE", 403);
+    }
+};
+
+export default checkRole;
